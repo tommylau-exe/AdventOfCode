47d8da559f88174204d0f70ca9323516dd88a36f
Thomas Lauerman <tom@lauerman.dev>
2023-12-01 17:47:52 -0800

    2023 day 1 clean-up

--- Sources/AdventOfCode/2023/Year2023+Day1.swift
+++ Sources/AdventOfCode/2023/Year2023+Day1.swift
@@ -15,8 +15,7 @@ extension Year2023 {
 extension Year2023.Day1 {
     static func part1(input: String) -> Int {
         input
-            .trimmingCharacters(in: .whitespacesAndNewlines)
-            .components(separatedBy: .newlines)
+            .split(whereSeparator: \.isNewline)
             .map { $0.compactMap(\.wholeNumberValue) }
             .map { $0.first! * 10 + $0.last! }
             .reduce(0, +)
@@ -36,36 +35,32 @@ extension Year2023.Day1 {
             "nine"
         }
         
-        let firstDigit = TryCapture(digit, transform: integer(from:))
+        let firstDigit = TryCapture(digit, transform: DigitParser.int(from:))
         
         let lastDigit = Regex {
             ZeroOrMore(.any)
-            TryCapture(digit, transform: integer(from:))
-        }
-        
-        func integer(from digitString: some StringProtocol) -> Int? {
-            if digitString.count == 1 {
-                return digitString.first?.wholeNumberValue
-            } else {
-                let formatter = NumberFormatter()
-                formatter.numberStyle = .spellOut
-                
-                return formatter.number(from: String(digitString))?.intValue
-            }
+            TryCapture(digit, transform: DigitParser.int(from:))
         }
         
         return input
-            .trimmingCharacters(in: .whitespacesAndNewlines)
-            .split(whereSeparator: \.isWhitespace)
+            .split(whereSeparator: \.isNewline)
             .map { line in
                 let firstDigit = line.firstMatch(of: firstDigit).map(\.output.1)!
                 let lastDigit = line.firstMatch(of: lastDigit).map(\.output.1)!
-                
-                return (firstDigit, lastDigit)
-            }
-            .map { firstDigit, lastDigit in
-                firstDigit * 10 + lastDigit
+                return firstDigit * 10 + lastDigit
             }
             .reduce(0, +)
+    }
+}
+
+private struct DigitParser {
+    private static let formatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .spellOut
+        return formatter
+    }()
+    
+    static func int(from digitString: some StringProtocol) -> Int? {
+        digitString.first?.wholeNumberValue ?? formatter.number(from: String(digitString))?.intValue
     }
 }
