d9a905a2b4f2cd37e7f543fc3928a802a2264ba0
Thomas Lauerman <tom@lauerman.dev>
2023-12-07 15:00:45 -0800

    2023 day 7 part 2 + cleanup

--- Sources/AdventOfCode/2023/Year2023+Day7.swift
+++ Sources/AdventOfCode/2023/Year2023+Day7.swift
@@ -19,85 +19,51 @@ extension Year2023.Day7 {
             " "
             Capture(OneOrMore(.digit)) { Int($0)! }
         }
+        let handComparator = HandComparator(
+            cardRankingStrategy: Part1.ranking,
+            handKindStrategy: Part1.kind
+        )
         
         return input.matches(of: bidPattern)
             .map(\.output)
-            .map { Hand(cards: Array($1), bid: $2) }
-            .sorted { $0.isStrongerThan($1) }
+            .map { Bid(hand: Hand(cards: Array($1)), amount: $2) }
+            .sorted { handComparator.isStrongerThan($0.hand, $1.hand) }
             .reversed()
             .enumerated()
             .map { ($0+1, $1) }
-            .map { $0 * $1.bid }
+            .map { $0 * $1.amount }
             .reduce(0, +)
     }
     
     static func part2(input: String) -> Int {
-        fatalError()
+        let bidPattern = Regex {
+            Capture(OneOrMore(.word))
+            " "
+            Capture(OneOrMore(.digit)) { Int($0)! }
+        }
+        let handComparator = HandComparator(
+            cardRankingStrategy: Part2.ranking,
+            handKindStrategy: Part2.kind
+        )
+        
+        return input.matches(of: bidPattern)
+            .map(\.output)
+            .map { Bid(hand: Hand(cards: Array($1)), amount: $2) }
+            .sorted { handComparator.isStrongerThan($0.hand, $1.hand) }
+            .reversed()
+            .enumerated()
+            .map { ($0+1, $1) }
+            .map { $0 * $1.amount }
+            .reduce(0, +)
     }
 }
 
 private typealias Card = Character
-
-private let cardRankings: [Card: Int] = Dictionary(
-    "AKQJT98765432".enumerated().map { ($1, $0) },
-    uniquingKeysWith: { first, _ in first }
-)
-
-private extension Card {
-    func isStrongerThan(_ other: Self) -> Bool {
-        cardRankings[self]! < cardRankings[other]!
-    }
-}
+private typealias CardRankingStrategy = (Card) -> Int
+private typealias HandKindStrategy = (Hand) -> Hand.Kind
 
 private struct Hand {
     let cards: [Card]
-    let bid: Int
-    
-    var kind: Kind {
-        let cardCounts: [Card: Int] = cards.reduce(into: [:]) { $0[$1] = ($0[$1] ?? 0) + 1 }
-        
-        if cardCounts.count == 1 {
-            return .fiveOfAKind
-        }
-        
-        if cardCounts.count == 2 && cardCounts.values.contains(where: { $0 == 4 }) {
-            return .fourOfAKind
-        }
-        
-        if cardCounts.count == 2 {
-            return .fullHouse
-        }
-        
-        if cardCounts.count == 3 && cardCounts.values.contains(where: { $0 == 3 }) {
-            return .threeOfAKind
-        }
-        
-        if cardCounts.count == 3 {
-            return .twoPair
-        }
-        
-        if cardCounts.count == 4 {
-            return .onePair
-        }
-        
-        return .highCard
-    }
-    
-    func isStrongerThan(_ other: Self) -> Bool {
-        let kind = kind
-        let otherKind = other.kind
-        
-        guard kind == otherKind else {
-            return kind.isStrongerThan(otherKind)
-        }
-        
-        for (card, otherCard) in zip(cards, other.cards) {
-            guard card != otherCard else { continue }
-            return card.isStrongerThan(otherCard)
-        }
-        
-        return false
-    }
 }
 
 private extension Hand {
@@ -112,6 +78,102 @@ private extension Hand {
         
         func isStrongerThan(_ other: Self) -> Bool {
             rawValue < other.rawValue
+        }
+    }
+}
+
+private struct Bid {
+    let hand: Hand
+    let amount: Int
+}
+
+private struct HandComparator {
+    let cardRankingStrategy: CardRankingStrategy
+    let handKindStrategy: HandKindStrategy
+    
+    func isStrongerThan(_ lhs: Hand, _ rhs: Hand) -> Bool {
+        let kind = handKindStrategy(lhs)
+        let otherKind = handKindStrategy(rhs)
+        
+        guard kind == otherKind else {
+            return kind.isStrongerThan(otherKind)
+        }
+        
+        for (card, otherCard) in zip(lhs.cards, rhs.cards) {
+            guard card != otherCard else { continue }
+            
+            let firstCardIsStronger = cardRankingStrategy(card) < cardRankingStrategy(otherCard)
+            return firstCardIsStronger
+        }
+        
+        return false
+    }
+}
+
+private enum Part1 {
+    private static let cardRankings: [Card: Int] = Dictionary(
+        "AKQJT98765432".enumerated().map { ($1, $0) },
+        uniquingKeysWith: { first, _ in first }
+    )
+    
+    static func ranking(for card: Card) -> Int {
+        cardRankings[card]!
+    }
+    
+    static func kind(for hand: Hand) -> Hand.Kind {
+        let cardCounts = hand.cards.grouped(by: { $0 }).values.map(\.count)
+        
+        return switch (cardCounts.count) {
+        case 1:
+            .fiveOfAKind
+        case 2 where cardCounts.contains(4):
+            .fourOfAKind
+        case 2:
+            .fullHouse
+        case 3 where cardCounts.contains(3):
+            .threeOfAKind
+        case 3:
+            .twoPair
+        case 4:
+            .onePair
+        default:
+            .highCard
+        }
+    }
+}
+
+private enum Part2 {
+    private static let cardRankings: [Card: Int] = Dictionary(
+        "AKQT98765432J".enumerated().map { ($1, $0) },
+        uniquingKeysWith: { first, _ in first }
+    )
+    
+    static func ranking(for card: Card) -> Int {
+        cardRankings[card]!
+    }
+    
+    static func kind(for hand: Hand) -> Hand.Kind {
+        guard hand.cards.contains(where: { $0 == "J" }) else {
+            return Part1.kind(for: hand)
+        }
+        
+        let cardCounts = hand.cards
+            .filter { $0 != "J" }
+            .grouped(by: { $0 })
+            .values
+            .map(\.count)
+        
+        return switch cardCounts.count {
+        case 0, 1:
+            .fiveOfAKind
+        case 2 where cardCounts.allSatisfy({ $0 == 2}):
+            .fullHouse
+        case 2:
+            .fourOfAKind
+        case 3:
+            .threeOfAKind
+        default:
+            .onePair
         }
     }
 }
